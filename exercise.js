964a941267f3d6ea0e47d662b7b56e0778889236
KCdelmo22 <98967207+KCdelmo22@users.noreply.github.com>
2022-03-16 16:36:38 +0800

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,32 @@
+//Javascript Arrays
+var myNumbers = [1, 2, 3, 4, 5]
+var colors = ["red", "yellow", "green", "blue", "orange"]
+
+console.log(colors[4]);
+
+// String
+var name = "joe"
+console.log( name[1] ) // "o"*/
+
+//if else statement
+if (5 > 3){
+    console.log("5 is greater than 3")
+}
+let cat = 3;
+if (cat){
+    console.log(cat + " is the length")
+}
+////////////////
+if ("cat" != "dog"){
+    console.log("not the same")
+} else {
+    console.log("they're equal")
+}
+//////////////////////////
+var age = 22;
+
+if (age >= 18) {
+    console.log("is allowed to go to the movie");
+}else{
+    console.log("isnt allowed to go to the movie");
+}
